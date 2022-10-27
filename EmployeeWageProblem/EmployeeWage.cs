ff586a4950f33564c79628010c0ec17e0ee62ee2
PrajaktaShewale <shewalep94@gmail.com>
2022-10-27 09:22:46 +0530

    [Prajakta]Add:CalWageMonth

--- EmployeeWageProblem/EmployeeWage.cs
+++ EmployeeWageProblem/EmployeeWage.cs
@@ -4,7 +4,7 @@ namespace EmployeeWageProblem
     public class EmployeeWage
     {
         const int WAGE_PER_HR=20,FULL_DAY_HR=8,PART_TIME_HR=4,IS_PRESENT=0,
-            IS_FULL_TIME=1,IS_PART_TIME=2;
+            IS_FULL_TIME=1,IS_PART_TIME=2, TOTAL_WORKING_DAYS=20;
         Random random = new Random();
         public void Attendance()
         {
@@ -17,16 +17,26 @@ namespace EmployeeWageProblem
         }
         public void EmpWage()
         { 
-        int dailyEmpWage = 0,empHrs=0;
-            int empCheck = random.Next(0, 3);//0,1,2
-            if (empCheck == IS_FULL_TIME)
-                empHrs = FULL_DAY_HR;
-            else if (empCheck == IS_PART_TIME)
-                empHrs = PART_TIME_HR;
-            else
-                empHrs = 0;
-            dailyEmpWage = WAGE_PER_HR * empHrs;
-            Console.WriteLine("Daily Emp Wage" + dailyEmpWage);
+        int totalEmpWage = 0,empHrs=0;
+            for (int i = 0; i < TOTAL_WORKING_DAYS; i++)
+            {
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS_FULL_TIME:
+                        empHrs +=FULL_DAY_HR;
+                        break;
+                        case IS_PART_TIME:      
+                        empHrs +=PART_TIME_HR;
+                        break ;
+                        default:
+                        empHrs += 0;
+                        break;
+                }
+            }
+            
+            totalEmpWage = WAGE_PER_HR * empHrs;
+            Console.WriteLine("Daily Emp Wage" + totalEmpWage);
 
         }
 

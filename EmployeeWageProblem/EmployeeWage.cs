ca17dbb514a6f8075b8e4d947577e29c476cce95
PrajaktaShewale <shewalep94@gmail.com>
2022-11-08 22:09:59 +0530

    [Prajakta]Add:CalculateDailyEmplyeeWage

--- EmployeeWageProblem/EmployeeWage.cs
+++ EmployeeWageProblem/EmployeeWage.cs
@@ -3,40 +3,23 @@ namespace EmployeeWageProblem
 {
     public class EmployeeWage
     {
-        const int WAGE_PER_HR=20,FULL_DAY_HR=8,PART_TIME_HR=4,IS_PRESENT=0,
-            IS_FULL_TIME=1,IS_PART_TIME=2, TOTAL_WORKING_DAYS=20;
+        const int WAGE_PER_HR=20,FULL_DAY_HR=8;
         Random random = new Random();
         public void Attendance()
         {
-            Random random = new Random();//inbuild class and has methods
-            int empCheck = random.Next(0,2);//0,1
+            int empCheck = random.Next(0,2);
             if (empCheck == 0)
                 Console.WriteLine("Employee is Present");
             else
                 Console.WriteLine("Employee is Absent");
         }
         public void EmpWage()
-        { 
-        int totalEmpWage = 0,empHrs=0;
-            for (int i = 0; i < TOTAL_WORKING_DAYS; i++)
-            {
-                int empCheck = random.Next(0, 3);
-                switch (empCheck)
-                {
-                    case IS_FULL_TIME:
-                        empHrs +=FULL_DAY_HR;
-                        break;
-                        case IS_PART_TIME:      
-                        empHrs +=PART_TIME_HR;
-                        break ;
-                        default:
-                        empHrs += 0;
-                        break;
-                }
-            }
-            
-            totalEmpWage = WAGE_PER_HR * empHrs;
-            Console.WriteLine("Daily Emp Wage" + totalEmpWage);
+        {
+            int totalEmpWage = 0;
+                int empCheck = random.Next(0, 2);
+            if (empCheck == 0)            
+            totalEmpWage = WAGE_PER_HR * FULL_DAY_HR;
+            Console.WriteLine("Daily Emp Wage is " + totalEmpWage);
 
         }
 
